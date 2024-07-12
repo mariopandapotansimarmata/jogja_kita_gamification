38fd557fc9662dbfcd9de2c1a2b929c0019d9692
mariopandapotansimarmata <kurcisandiwara@gmail.com>
2024-07-12 21:27:37 +0700

    add resto recommendation, and nearby resto

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -75,7 +75,7 @@ class _HomePageState extends State<HomePage> {
                     ],
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
                 const Padding(
@@ -136,13 +136,123 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ],
                 ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Promo di sekitar anda",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w700),
+                      ),
+                      Text("Lihat Semua",
+                          style: TextStyle(fontSize: 14, color: Colors.red))
+                    ],
+                  ),
+                ),
+                RecomendationResto(),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Resto Terdekat",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w700),
+                      ),
+                      Text("Lihat Semua",
+                          style: TextStyle(fontSize: 14, color: Colors.red))
+                    ],
+                  ),
+                ),
+                RecomendationResto()
               ],
             ),
             const LocationWidget(),
             const BonusCard()
           ],
         ),
+        const SizedBox(
+          height: 40,
+        )
       ]),
+    );
+  }
+}
+
+class RecomendationResto extends StatelessWidget {
+  const RecomendationResto({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // Promo di sekitar anda
+      padding: const EdgeInsets.only(left: 8),
+      height: 235,
+      child: ListView.builder(
+        itemCount: 5,
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 2,
+                    offset: Offset(0.5, 1), // Shadow position
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(color: Colors.grey)),
+            margin: const EdgeInsets.all(5),
+            height: 245,
+            width: 160,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  clipBehavior: Clip.antiAlias,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(16)),
+                  // height: 155,
+                  // width: 147,
+                  child: const Image(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/recomendation.png')),
+                ),
+                const Expanded(
+                  child: Text(
+                    "Ayam Bakar P Widodo Galih, Wahid Hasyim, C",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                const Row(
+                  children: [
+                    Icon(
+                      Icons.location_pin,
+                      color: Color(0xffCA110F),
+                    ),
+                    Text("3 KM")
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
