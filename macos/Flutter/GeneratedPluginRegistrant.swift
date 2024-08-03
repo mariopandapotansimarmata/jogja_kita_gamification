fc5b3bbf6b30f2ddcd60f81bd4492ea83703ffc5
mariopandapotansimarmata <kurcisandiwara@gmail.com>
2024-08-03 16:08:38 +0700

    resolve unnecessary dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
-import flutter_barcode_sdk
 import geolocator_apple
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FlutterBarcodeSdkPlugin.register(with: registry.registrar(forPlugin: "FlutterBarcodeSdkPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
